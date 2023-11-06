1345efc9f53fd5c44a9dac109099df1f9a260d44
Tamizharasan R <134991180+Tamizh2R@users.noreply.github.com>
2023-11-06 14:08:47 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "YOUR_RDS_ENDPOINT";
+$username = "YOUR_DB_USER";
+$password = "YOUR_DB_PASSWORD";
+$dbname = "YOUR_DB_NAME";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO contact_form (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
