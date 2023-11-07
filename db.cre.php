35bd2874e2b8972b801c721bf387938efe312049
Tamizharasan R <134991180+Tamizh2R@users.noreply.github.com>
2023-11-07 15:10:53 +0530

    Create db.cre.php

---
+++ db.cre.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'database-2.codee3as2cnl.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'tamizh');
+define('DB_PASSWORD', 'tamizh001');
+define('DB_DATABASE', 'php');
+?>
