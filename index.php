75716813f762041d8dbdb3287823d49868661002
Tamizharasan R <134991180+Tamizh2R@users.noreply.github.com>
2023-11-07 15:12:18 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Store Name and Age</title>
+</head>
+<body>
+    <h2>Enter Name and Age:</h2>
+    <form method="post" action="process.php">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br><br>
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age" required>
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
